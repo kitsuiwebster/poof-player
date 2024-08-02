b26c8ee0571dfbdfb0b9eeb9aa47c16efe5402c7
kitsuiwebster <raphmartin1999@gmail.com>
2024-08-02 23:42:04 +0200

    feat: bot is up

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use serenity::async_trait;
+use serenity::model::gateway::Ready;
+use serenity::model::prelude::*;
+use serenity::prelude::*;
+use dotenv::dotenv;
+use std::env;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+
+    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::GUILD_VOICE_STATES;
+
+    let mut client = Client::builder(&token, intents)
+        .event_handler(Handler)
+        .await
+        .expect("Err creating client");
+
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
 }
